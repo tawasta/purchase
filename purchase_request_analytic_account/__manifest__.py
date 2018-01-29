084bb6b88588388ac1aa58779349f7eb0409157d
Timo Talvitie <timo.talvitie@vizucom.com>
2018-01-29 12:25:37 +0200

    Transfer top-level analytic account info from PR to PO

--- purchase_request_analytic_account/__manifest__.py
+++ purchase_request_analytic_account/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Analytic Account for Purchase Request',
     'summary': 'New field for indicating what AA the PR is related to',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Purchases',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
@@ -34,8 +34,7 @@
         'bin': [],
     },
     'depends': [
-        'project',
-        'purchase_request',
+        'purchase_request_to_rfq',
     ],
     'data': [
         'views/purchase_request.xml',
