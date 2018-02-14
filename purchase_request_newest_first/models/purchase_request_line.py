8e6654aed79c2a643f032b0b7dd23aa219d5f935
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-02-14 12:10:57 +0200

    New module purchase_request_newest_first

---
+++ purchase_request_newest_first/models/purchase_request_line.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import api, fields, models, exceptions, _
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class PurchaseRequestLine(models.Model):
+    
+    # 1. Private attributes
+    _inherit = 'purchase.request.line'
+    _order = 'request_id ASC,id ASC'
+
+    # 2. Fields declaration
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
