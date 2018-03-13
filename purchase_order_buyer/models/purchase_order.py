344b2b62a6c34926e4a02fd4cbb870cc48e95d31
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-13 21:26:04 +0200

    New module purchase_order_buyer

---
+++ purchase_order_buyer/models/purchase_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    def _get_buyer_id(self):
+        return self.env.uid
+
+    buyer_id = fields.Many2one(
+        default=_get_buyer_id,
+        comodel_name='res.users',
+        string="Buyer"
+    )
