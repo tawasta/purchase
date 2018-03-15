e72cb281fcf7d131b34b40a4d0febbbb9f257dfb
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-15 09:46:11 +0200

    New module purchase_request_analytic_account_location

---
+++ purchase_request_analytic_account_location/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2018 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': 'Analytic account stock location for purchase requests',
+    'summary': 'Adds analytic account stock location to PR',
+    'version': '10.0.1.0.0',
+    'category': 'Purchases',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'purchase_request_analytic_account',
+        'stock_location_analytic_account',
+    ],
+    'data': [
+        'views/purchase_request.xml',
+    ],
+    'demo': [
+    ],
+}
