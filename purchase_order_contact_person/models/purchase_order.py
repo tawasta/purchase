9339425dfb498d1fc3d7e58d62b1ec6c6bbe9087
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-27 23:14:12 +0300

    Improve formatting

--- purchase_order_contact_person/models/purchase_order.py
+++ purchase_order_contact_person/models/purchase_order.py
@@ -7,6 +7,6 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     supplier_contact_id = fields.Many2one(
-        comodel_name='res.partner', 
+        comodel_name='res.partner',
         string="Vendor's contact"
     )
