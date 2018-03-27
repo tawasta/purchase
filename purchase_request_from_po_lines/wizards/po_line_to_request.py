6c4a74caaa0f3094728835891a4c982763998a83
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-27 22:27:24 +0300

    Improve formatting

--- purchase_request_from_po_lines/wizards/po_line_to_request.py
+++ purchase_request_from_po_lines/wizards/po_line_to_request.py
@@ -14,15 +14,15 @@ class PoLineToRequest(models.TransientModel):
 
         if self.type == 'latest_own':
             res = purchase_request_model.search(
-                args=[('requested_by', '=', self.env.uid), 
-                      ('state', 'in', ['draft', 'to_approve'])], 
-                limit=1, 
+                args=[('requested_by', '=', self.env.uid),
+                      ('state', 'in', ['draft', 'to_approve'])],
+                limit=1,
                 order='date_start DESC, id DESC')
 
             if not res:
                 self.purchase_request_id = False
                 self.type = 'new'
-                txt = _("You don't have any Purchase Requests " \
+                txt = _("You don't have any Purchase Requests "
                         "that are in Draft or To Approve state.")
                 raise exceptions.Warning(txt)
             else:
@@ -43,12 +43,13 @@ class PoLineToRequest(models.TransientModel):
                 'description': _('Created from Purchase Order Lines')
             }
 
-            # If purchase_request_analytic_account module is installed, 
+            # If purchase_request_analytic_account module is installed,
             # add analytic account also to purchase request, not just the lines
             if hasattr(purchase_request_model, 'analytic_account_id'):
-                pr_vals['analytic_account_id'] = self.analytic_account_id and \
-                                                 self.analytic_account_id.id or \
-                                                 False
+                pr_vals['analytic_account_id'] \
+                    = self.analytic_account_id and \
+                    self.analytic_account_id.id or \
+                    False
 
             pr_res = purchase_request_model.create(pr_vals)
 
@@ -56,11 +57,11 @@ class PoLineToRequest(models.TransientModel):
             pr_res = self.purchase_request_id
 
         for po_line in purchase_order_line_model.browse(
-            self.env.context['active_ids']):
+                self.env.context['active_ids']):
             purchase_request_line_model.create({
                 'request_id': pr_res.id,
-                'analytic_account_id': self.analytic_account_id and \
-                    self.analytic_account_id.id or False,
+                'analytic_account_id': self.analytic_account_id and
+                self.analytic_account_id.id or False,
                 'product_id': po_line.product_id.id,
                 'product_uom_id': po_line.product_uom.id,
                 'product_qty': po_line.product_qty,
@@ -80,53 +81,53 @@ class PoLineToRequest(models.TransientModel):
             return {}
 
     purchase_request_id = fields.Many2one(
-        comodel_name='purchase.request', 
-        string='Purchase Request', 
-        domain=[('state', 'in', ['draft', 'to_approve'])], 
+        comodel_name='purchase.request',
+        string='Purchase Request',
+        domain=[('state', 'in', ['draft', 'to_approve'])],
         help='Purchase Requests in Draft or To Approve state'
     )
-    
+
     analytic_account_id = fields.Many2one(
-        comodel_name='account.analytic.account', 
-        string='Analytic Account', 
-        help='Analytic Account to be used when creating a new Purchase ' \
-            'Request or adding lines to an existing one'
+        comodel_name='account.analytic.account',
+        string='Analytic Account',
+        help='Analytic Account to be used when creating a new Purchase '
+             'Request or adding lines to an existing one'
     )
     type = fields.Selection(
-        [('new', 'Create New Purchase Request'), 
-         ('latest_own', 'Add to Latest own Purchase Request'), 
-         ('other', 'Add to Other Existing Purchase Request')], 
+        [('new', 'Create New Purchase Request'),
+         ('latest_own', 'Add to Latest own Purchase Request'),
+         ('other', 'Add to Other Existing Purchase Request')],
         string='Type',
-        required=True, 
-        default='new', 
-        help='Choose whether you want to create a new Purchase Request ' \
+        required=True,
+        default='new',
+        help='Choose whether you want to create a new Purchase Request '
              'or add lines to an existing one'
     )
     redirect_to_purchase_request = fields.Boolean(
-        string='Show Purchase Request Form', 
+        string='Show Purchase Request Form',
         default=False
     )
     # Some related fields from selected purchase request account,
     # for quick access to relevant info
     requested_by = fields.Many2one(
-        comodel_name='res.users', 
-        related='purchase_request_id.requested_by', 
-        string='Requested By', 
+        comodel_name='res.users',
+        related='purchase_request_id.requested_by',
+        string='Requested By',
         readonly=True
     )
     assigned_to = fields.Many2one(
-        comodel_name='res.users', 
-        related='purchase_request_id.assigned_to', 
-        string='Approver', 
+        comodel_name='res.users',
+        related='purchase_request_id.assigned_to',
+        string='Approver',
         readonly=True
     )
     date_start = fields.Date(
-        related='purchase_request_id.date_start', 
-        string='Creation date', 
+        related='purchase_request_id.date_start',
+        string='Creation date',
         readonly=True
     )
     description = fields.Text(
-        related='purchase_request_id.description', 
-        string='Description', 
+        related='purchase_request_id.description',
+        string='Description',
         readonly=True
     )
