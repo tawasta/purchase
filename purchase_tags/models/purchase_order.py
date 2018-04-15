f76f65b69db603032e0a9bbb4669da171b439f4e
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-04-15 17:39:42 +0300

    New module purchase_order_tags

---
+++ purchase_tags/models/purchase_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    tag_ids = fields.Many2many(
+        comodel_name='purchase_tags.tag',
+        string='Tags'
+    )
