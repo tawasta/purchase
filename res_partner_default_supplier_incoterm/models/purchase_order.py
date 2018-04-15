b167c9e2d2fd706e47b66fa46438ee9ccffc5bdd
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-04-15 18:43:29 +0300

    New module res_partner_default_supplier_incoterm

---
+++ res_partner_default_supplier_incoterm/models/purchase_order.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    @api.model
+    def create(self, vals):
+        if 'incoterm_id' not in vals and 'partner_id' in vals:
+            partner = self.env['res.partner'].browse(vals['partner_id'])
+            if partner and partner.supplier_incoterm_id:
+                vals['incoterm_id'] = partner.supplier_incoterm_id.id
+
+        return super(PurchaseOrder, self).create(vals)
+
+    @api.onchange('partner_id')
+    def onchange_partner_id_incoterm(self):
+        if self.partner_id and self.partner_id.supplier_incoterm_id:
+            self.incoterm_id = self.partner_id.supplier_incoterm_id.id
---
+++ res_partner_default_supplier_incoterm/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+
+    _inherit = 'res.partner'
+
+    supplier_incoterm_id = fields.Many2one(
+        comodel_name='stock.incoterms',
+        string='Supplier Incoterm',
+        company_dependent=True
+    )
