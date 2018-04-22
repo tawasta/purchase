883e965f667edd0b0b6c9a88a3fb2093b8c38208
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-04-22 23:25:33 +0300

    New module purchase_order_line_running_number

---
+++ purchase_order_line_running_number/models/purchase_order_line.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class PurchaseOrderLine(models.Model):
+
+    _inherit = 'purchase.order.line'
+
+    @api.multi
+    def _compute_running_number(self):
+        '''Assigns a running number to the PO line, starting from 1 onwards '''
+        for line in self:
+            po_lines = self.search(args=[('order_id', '=', line.order_id.id)],
+                                   order='id')
+
+            i = 1
+            for po_line in po_lines:
+                if po_line.id == line.id:
+                    line.running_number = i
+                    break
+                i += 1
+
+    running_number = fields.Integer(
+        compute=_compute_running_number,
+        string='Running Number'
+    )
