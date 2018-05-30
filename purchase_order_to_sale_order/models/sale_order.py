492a9f364d1d3330126c065bbc545c5b1a2ef143
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-05-30 14:48:05 +0300

    Add a new module to provide a shortcut for creating SO from PO

---
+++ purchase_order_to_sale_order/models/sale_order.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    purchase_order_ids = fields.One2many(
+        string='Purchase order',
+        comodel_name='purchase.order',
+        inverse_name='sale_order_id',
+    )
