e41a23c13c3b7399c9c90e610bdefe901d160b06
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-02 21:02:33 +0300

    Add a new module for using project analytic tags on purchase order lines

---
+++ purchase_project_analytic_tags/models/purchase_order_line.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+
+    _inherit = 'purchase.order.line'
+
+    @api.onchange('account_analytic_id')
+    def onchange_account_analytic_id_update_analytic_tags(self):
+        for record in self:
+            if record.account_analytic_id and \
+                    record.account_analytic_id.tag_ids:
+
+                record.analytic_tag_ids = record.account_analytic_id.tag_ids
