1c452000fca88ffe85290f4eb7d6b4242c17a444
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-09-25 10:56:48 +0300

    New module purchase_order_end_customer

---
+++ purchase_order_end_customer/models/purchase_order.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    end_customer_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='End customer',
+        domain=[('customer', '=', True)],
+        help='The end customer this purchase order is related to'
+    )
