0e4e886dfa3a82b4132c6ec21e031591e8864c4e
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-10-15 12:42:34 +0300

    Fix used field's name

--- purchase_order_weight/models/purchase_order_line.py
+++ purchase_order_weight/models/purchase_order_line.py
@@ -38,8 +38,8 @@ class PurchaseOrderLine(models.Model):
             )
 
             # Negative quantity doesn't have a weight
-            uom_qty = record.product_uom_qty \
-                if record.product_uom_qty > 0 else 0
+            uom_qty = record.product_qty \
+                if record.product_qty > 0 else 0
 
             weight = uom_qty * uom_weight
 
