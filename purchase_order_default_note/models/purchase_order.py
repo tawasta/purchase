5898c4ffa340c5c22b700fc6ffef46fad60b11a2
difbit <timo.kekalainen@hotmail.com>
2019-04-01 14:38:42 +0300

    [IMP] Onchange takes different parameter now

--- purchase_order_default_note/models/purchase_order.py
+++ purchase_order_default_note/models/purchase_order.py
@@ -8,7 +8,7 @@ class PurchaseOrder(models.Model):
 
     _inherit = 'purchase.order'
 
-    @api.onchange('partner_id')
+    @api.onchange('payment_term_id')
     def onchange_partner_id_update_purchase_note(self):
         for record in self:
             if record.partner_id and record.partner_id.purchase_note:
