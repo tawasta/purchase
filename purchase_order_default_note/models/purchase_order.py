95d24b0adda68e9d67976b0e755ba8f0277cbadd
difbit <timo.kekalainen@hotmail.com>
2019-04-01 15:47:24 +0300

    [IMP] A better onchange

--- purchase_order_default_note/models/purchase_order.py
+++ purchase_order_default_note/models/purchase_order.py
@@ -8,7 +8,7 @@ class PurchaseOrder(models.Model):
 
     _inherit = 'purchase.order'
 
-    @api.onchange('currency_id')
+    @api.onchange('fiscal_position')
     def onchange_partner_id_update_purchase_note(self):
         for record in self:
             if record.partner_id and record.partner_id.purchase_note:
