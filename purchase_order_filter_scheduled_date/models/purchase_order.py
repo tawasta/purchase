3ad3f23eb74a73f51da06eacc05a3825ea04f9a9
difbit <timo.kekalainen@hotmail.com>
2019-05-10 16:53:53 +0300

    [IMP] Fixed whitespaces and renamed a function

--- purchase_order_filter_scheduled_date/models/purchase_order.py
+++ purchase_order_filter_scheduled_date/models/purchase_order.py
@@ -18,13 +18,13 @@ class PurchaseOrder(models.Model):
                             order.has_been_shipped = True
 
     @api.multi
-    def _value_search(self,operator,value):
-        recs = self.search([]).filtered(lambda x:x.has_been_shipped is False)
+    def _search_value(self, operator, value):
+        recs = self.search([]).filtered(lambda x: x.has_been_shipped is False)
         if recs:
-            return [('id','in',[x.id for x in recs])]
+            return [('id', 'in', [x.id for x in recs])]
 
     has_been_shipped = fields.Boolean(
         string='Delivered',
         compute=_get_picking_state,
-        search='_value_search'
+        search='_search_value'
         )
