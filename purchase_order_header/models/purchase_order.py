12255d1898da77d49b1463bd2502d0b7ed3b2c3d
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2019-11-19 14:23:14 +0200

    [ADD] New module for PO header text (#18)

---
+++ purchase_order_header/models/purchase_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    header_text = fields.Char(
+        string='Header',
+        help='Header or title of the Purchase'
+    )
