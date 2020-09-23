738bb348caad227c5d6468a562a64eab8399e6c3
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-09-23 13:43:53 +0300

    [MIG] 12.0 res_partner_default_supplier_incoterm

--- res_partner_default_supplier_incoterm/models/res_partner.py
+++ res_partner_default_supplier_incoterm/models/res_partner.py
@@ -3,10 +3,10 @@ from odoo import models, fields
 
 class ResPartner(models.Model):
 
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     supplier_incoterm_id = fields.Many2one(
-        comodel_name='stock.incoterms',
-        string='Supplier Incoterm',
-        company_dependent=True
+        comodel_name="account.incoterms",
+        string="Supplier Incoterm",
+        company_dependent=True,
     )
