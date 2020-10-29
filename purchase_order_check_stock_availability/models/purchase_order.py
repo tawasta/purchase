4f159dfc9502fdfdabba411fe7de7dee02ebb750
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-10-29 12:35:28 +0200

    [REF] Formatting

--- purchase_order_check_stock_availability/models/purchase_order.py
+++ purchase_order_check_stock_availability/models/purchase_order.py
@@ -1,44 +1,24 @@
-
-# 1. Standard library imports:
-
-# 2. Known third party imports:
-
-# 3. Odoo imports:
 from odoo import fields, models
 
-# 4. Imports from Odoo modules:
+import logging
 
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
+_logger = logging.getLogger(__name__)
 
 
 class PurchaseOrder(models.Model):
 
-    # 1. Private attributes
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
-    # 2. Fields declaration
     availability_line_ids = fields.One2many(
-        comodel_name='purchase.order.availability.line',
-        inverse_name='order_id',
-        string='Available Products',
+        comodel_name="purchase.order.availability.line",
+        inverse_name="order_id",
+        string="Available Products",
     )
 
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-
-    # 5. Constraints and onchanges
-
-    # 6. CRUD methods
-
-    # 7. Action methods
     def check_stock_availability(self):
 
-        stock_location_model = self.env['stock.location']
-        availability_line_model \
-            = self.env['purchase.order.availability.line']
+        stock_location_model = self.env["stock.location"]
+        availability_line_model = self.env["purchase.order.availability.line"]
 
         # Clear current lines
         self.availability_line_ids = False
@@ -50,26 +30,30 @@ class PurchaseOrder(models.Model):
         # Iterate all lines on the PR, check their available quantities in
         # different stock locations, and create availability lines accordingly
         for order_line in self.order_line:
+
             for location in locations_to_check:
-                qty_available = order_line.product_id \
-                    .with_context(location=location.id).qty_available
+                qty_available = order_line.product_id.with_context(
+                    location=location.id
+                ).qty_available
 
                 if qty_available > 0:
-                    availability_line_model.create({
-                        'order_id': self.id,
-                        'order_line_id': order_line.id,
-                        'available_qty': qty_available,
-                        'location_id': location.id,
-                    })
+                    availability_line_model.create(
+                        {
+                            "order_id": self.id,
+                            "order_line_id": order_line.id,
+                            "available_qty": qty_available,
+                            "location_id": location.id,
+                        }
+                    )
 
-    # 8. Business methods
     def get_stock_location_domain(self):
-        ''' What stock locations should be included when checking if materials
+        """ What stock locations should be included when checking if materials
         exist elsewhere. By default included ones are internal physical
         locations that are linked to an analytic account (i.e. are project
         locations) and belong to the same company. The location set on the
         purchase order is filtered out to avoid suggesting same source/target
-        locations'''
-        return [('company_id', '=', self.company_id.id),
-                ('usage', '=', 'internal'),
-                ]
+        locations"""
+        return [
+            ("company_id", "=", self.company_id.id),
+            ("usage", "=", "internal"),
+        ]
--- purchase_order_check_stock_availability/models/purchase_order_availability_line.py
+++ purchase_order_check_stock_availability/models/purchase_order_availability_line.py
@@ -1,126 +1,85 @@
-
-# 1. Standard library imports:
-
-# 2. Known third party imports:
-
-# 3. Odoo imports:
 from odoo import api, fields, models, exceptions, _
-
-# 4. Imports from Odoo modules:
 import odoo.addons.decimal_precision as dp
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
 
 
 class PurchaseOrderAvailabilityLine(models.Model):
 
-    # 1. Private attributes
-    _name = 'purchase.order.availability.line'
-    _description = 'Purchase Order Availability Line'
-    _order = 'order_id, product_id'
-    _rec_name = 'order_line_id'
+    _name = "purchase.order.availability.line"
+    _description = "Purchase Order Availability Line"
+    _order = "order_id, product_id"
+    _rec_name = "order_line_id"
 
-    # 2. Fields declaration
     order_line_id = fields.Many2one(
-        comodel_name='purchase.order.line',
-        string='Related Order Line',
+        comodel_name="purchase.order.line", string="Related Order Line",
     )
     product_id = fields.Many2one(
-        related='order_line_id.product_id',
-        comodel_name='product.product',
-        string='Product'
+        related="order_line_id.product_id",
+        comodel_name="product.product",
+        string="Product",
     )
 
     product_qty = fields.Float(
-        related='order_line_id.product_qty',
-        string='Quantity wanted',
-        digits=dp.get_precision('Product Unit of Measure')
+        related="order_line_id.product_qty",
+        string="Quantity wanted",
+        digits=dp.get_precision("Product Unit of Measure"),
     )
 
     analytic_account_id = fields.Many2one(
-        related='order_line_id.account_analytic_id',
-        comodel_name='account.analytic.account',
-        string='Analytic Account',
+        related="order_line_id.account_analytic_id",
+        comodel_name="account.analytic.account",
+        string="Analytic Account",
     )
 
     product_uom_id = fields.Many2one(
-        related='order_line_id.product_uom',
-        comodel_name='uom.uom',
-        string='UoM'
+        related="order_line_id.product_uom", comodel_name="uom.uom", string="UoM"
     )
 
     available_qty = fields.Float(
-        string='Quantity at location',
-        digits=dp.get_precision('Product Unit of Measure')
+        string="Quantity at location",
+        digits=dp.get_precision("Product Unit of Measure"),
     )
 
     available_uom_id = fields.Many2one(
-        related='order_line_id.product_id.uom_id',
-        comodel_name='uom.uom',
-        string='UoM'
+        related="order_line_id.product_id.uom_id", comodel_name="uom.uom", string="UoM"
     )
 
-    order_id = fields.Many2one(
-        comodel_name='purchase.order',
-        string='Purchase Order',
-    )
+    order_id = fields.Many2one(comodel_name="purchase.order", string="Purchase Order",)
 
-    location_id = fields.Many2one(
-        comodel_name='stock.location',
-        string='Location'
-    )
+    location_id = fields.Many2one(comodel_name="stock.location", string="Location")
 
     location_dest_id = fields.Many2one(
-        comodel_name='stock.location',
-        string='Destination',
-        related='order_line_id.location_dest_id'
+        comodel_name="stock.location",
+        string="Destination",
+        related="order_line_id.location_dest_id",
     )
 
     company_id = fields.Many2one(
-        related='order_id.company_id',
-        comodel_name='res.company',
-        string='Company'
+        related="order_id.company_id", comodel_name="res.company", string="Company"
     )
 
-    active = fields.Boolean(
-        default=True,
-        string='Active'
-    )
+    active = fields.Boolean(default=True, string="Active")
 
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-
-    # 5. Constraints and onchanges
-
-    # 6. CRUD methods
-
-    # 7. Action methods
     @api.multi
     def create_transfer(self):
-        ''' Creates a new stock picking for transfering raw materials
-        to the location defined in the PR '''
+        """ Creates a new stock picking for transfering raw materials
+        to the location defined in the PR """
         self.ensure_one()
 
-        if not self.location_id or \
-                not self.location_dest_id:
+        if not self.location_id or not self.location_dest_id:
             raise exceptions.UserError(
-                _('Please set source and destination location first')
+                _("Please set source and destination location first")
             )
 
-        stock_picking_model = self.env['stock.picking']
-        stock_move_model = self.env['stock.move']
+        stock_picking_model = self.env["stock.picking"]
+        stock_move_model = self.env["stock.move"]
 
         vals = {
-            'purchase_id': self.order_id.id,
-            'picking_type_id': self._get_picking_type_for_transfer(),
-            'location_id': self.location_id.id,
-            'location_dest_id': self.location_dest_id.id,
-            'origin': '%s: %s %s' % (self.order_id.name,
-                                     _('Internal transfer of'),
-                                     self.product_id.name)
+            "purchase_id": self.order_id.id,
+            "picking_type_id": self._get_picking_type_for_transfer(),
+            "location_id": self.location_id.id,
+            "location_dest_id": self.location_dest_id.id,
+            "origin": "%s: %s %s"
+            % (self.order_id.name, _("Internal transfer of"), self.product_id.name),
         }
 
         res = stock_picking_model.create(vals)
@@ -128,16 +87,18 @@ class PurchaseOrderAvailabilityLine(models.Model):
         # TODO: UOM handling when using different units of measure
         transfer_qty = self._get_transfer_qty()
 
-        stock_move_model.create({
-            'name': self.product_id.name,
-            'picking_id': res.id,
-            'purchase_line_id': self.order_line_id.id,
-            'product_id': self.product_id.id,
-            'product_uom': self.product_uom_id.id,
-            'product_uom_qty': transfer_qty,
-            'location_id': res.location_id.id,
-            'location_dest_id': res.location_dest_id.id,
-        })
+        stock_move_model.create(
+            {
+                "name": self.product_id.name,
+                "picking_id": res.id,
+                "purchase_line_id": self.order_line_id.id,
+                "product_id": self.product_id.id,
+                "product_uom": self.product_uom_id.id,
+                "product_uom_qty": transfer_qty,
+                "location_id": res.location_id.id,
+                "location_dest_id": res.location_dest_id.id,
+            }
+        )
 
         if self.product_qty <= transfer_qty:
             # The whole qty is expected to be transferred, so remove all
@@ -155,35 +116,34 @@ class PurchaseOrderAvailabilityLine(models.Model):
             self.order_line_id.product_qty -= transfer_qty
 
         return {
-            'name': '%s / Internal transfers' % self.order_id.name,
-            'view_type': 'form',
-            'view_mode': 'form, tree',
-            'res_model': 'stock.picking',
-            'type': 'ir.actions.act_window',
-            'target': 'current',
-            'res_id': res.id,
+            "name": "%s / Internal transfers" % self.order_id.name,
+            "view_type": "form",
+            "view_mode": "form, tree",
+            "res_model": "stock.picking",
+            "type": "ir.actions.act_window",
+            "target": "current",
+            "res_id": res.id,
         }
 
-    # 8. Business methods
     def _get_picking_type_for_transfer(self):
-        '''What picking type should be used for the new stock picking.
+        """ What picking type should be used for the new stock picking.
         Default is an internal tranfer for the company of the purchase
-        orders'''
+        orders"""
         args = [
-            ('code', '=', 'internal'),
-            ('warehouse_id.company_id', '=', self.order_id.company_id.id)
+            ("code", "=", "internal"),
+            ("warehouse_id.company_id", "=", self.order_id.company_id.id),
         ]
 
-        res = self.env['stock.picking.type'].search(args, limit=1)
+        res = self.env["stock.picking.type"].search(args, limit=1)
 
         if not res:
-            raise exceptions.UserError(_('Stock picking type not found!'))
+            raise exceptions.UserError(_("Stock picking type not found!"))
         else:
             return res[0].id
 
     def _get_transfer_qty(self):
-        '''How much of the product should be suggested to be transferred
+        """How much of the product should be suggested to be transferred
         on the picking. Default is:
         -Total order quantity if available
-        -If total amount is not available, then transfer as much as possible'''
+        -If total amount is not available, then transfer as much as possible"""
         return min(self.available_qty, self.product_qty)
