fc2feadb2127fa68a3abdf996406f12a2c36f33d
Timo Kekäläinen <36134879+difbit@users.noreply.github.com>
2020-11-05 11:28:28 +0200

    [IMP] Get analytic account from items (#80)

--- purchase_request_analytic_account_to_po/wizards/purchase_request_line_make_purchase_order.py
+++ purchase_request_analytic_account_to_po/wizards/purchase_request_line_make_purchase_order.py
@@ -13,14 +13,9 @@ class PrLineMakePo(models.TransientModel):
                                                                 group_id,
                                                                 company,
                                                                 origin)
+        items = self.item_ids and self.item_ids[0]
 
-        # If the purchase order has a top-level analytic account field
-        # (from e.g.) purchase_order_analytic_account, set it to contain
-        # the purchase request's top-level analytic account
-        if hasattr(purchase_order_model, 'project_id') \
-                and self._context.get('active_id', None):
-            res['project_id'] = self.env['purchase.request.line'] \
-                .browse(self._context['active_id']) \
-                .request_id.analytic_account_id.id
+        if hasattr(purchase_order_model, 'project_id') and items:
+            res['project_id'] = items.request_id.analytic_account_id.id
 
         return res
