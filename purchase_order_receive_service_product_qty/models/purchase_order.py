2257e0791d2d02e4a374b906b1dd19745c5d29bd
difbit <timo.kekalainen@tawasta.fi>
2024-01-17 12:52:05 +0200

    [MIG] Migration of purchase_order_receive_service_product_qty module from 12.0 to 17.0 version

---
+++ purchase_order_receive_service_product_qty/models/purchase_order.py
@@ -0,0 +1,18 @@
+from odoo import models
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = "purchase.order"
+
+    def button_confirm(self):
+        result = super().button_confirm()
+
+        # Search for lines which have service as their product type
+        lines = (x for x in self.order_line if x.product_id.type == "service")
+
+        for line in lines:
+            # Set received qty the same as ordered qty
+            line.qty_received = line.product_qty
+
+        return result
