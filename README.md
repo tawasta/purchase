799bea55bdd108a26018713e42f1c8f213e761f9
difbit <timo.kekalainen@tawasta.fi>
2024-01-26 16:01:09 +0200

    [MIG] Migration of purchase_request_line_enable_approved_deletion module from 12.0 to 17.0 version

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ addon | version | maintainers | summary
 [purchase_order_select_tax](purchase_order_select_tax/) | 17.0.1.0.0 |  | Select tax from dropopwn-menu for Purchase Order Lines
 [purchase_order_show_dropship_address](purchase_order_show_dropship_address/) | 17.0.1.0.0 |  | Show the full address below the Drop Ship Address
 [purchase_order_standard_clause](purchase_order_standard_clause/) | 17.0.1.0.0 |  | Company-specific standard clause for POs
+[purchase_request_line_enable_approved_deletion](purchase_request_line_enable_approved_deletion/) | 17.0.1.0.0 |  | Allow deletion of Purchase request lines in approved-state
 [purchase_request_line_show_purchases](purchase_request_line_show_purchases/) | 17.0.1.0.0 |  | Show related purchase orders' numbers
 
 [//]: # (end addons)
---
+++ purchase_request_line_enable_approved_deletion/models/purchase_request_line.py
@@ -0,0 +1,31 @@
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class PurchaseRequestLine(models.Model):
+
+    _inherit = "purchase.request.line"
+
+    def _can_be_deleted(self):
+        """Redefines OCA's _can_be_deleted-method to allow deletion of
+        Purchase request lines if:
+        - Purchase request is in Draft- or Approved-state"""
+        self.ensure_one()
+        return self.request_state == "draft" or self.request_state == "approved"
+
+    def unlink(self):
+        """Overrides unlink-method and just the text shown in UserError after
+        _can_be_deleted is changed"""
+        if self.mapped("purchase_lines"):
+            raise UserError(
+                _("You cannot delete a record that refers to purchase " "lines!")
+            )
+        for line in self:
+            if not line._can_be_deleted():
+                raise UserError(
+                    _(
+                        "You can only delete a purchase request line if "
+                        "the purchase request is in draft or approved state."
+                    )
+                )
+        return super(PurchaseRequestLine, self).unlink()
