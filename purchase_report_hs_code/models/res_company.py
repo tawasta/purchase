fbb4be6cbe6b131d7123250a4236b9360fe15b70
difbit <timo.kekalainen@tawasta.fi>
2024-11-14 14:43:02 +0200

    [IMP] purchase_report_hs_code module: Settings to use HS codes directly from PO lines and to show HS codes per PO

--- purchase_report_hs_code/models/res_company.py
+++ purchase_report_hs_code/models/res_company.py
@@ -9,3 +9,7 @@ class ResCompany(models.Model):
         string="Always show HS code on print",
         default=False,
     )
+    use_po_line_hs_code = fields.Boolean(
+        string="Use HS codes directly from PO lines",
+        default=False,
+    )
