62bc9849965c25384ea85f74cad32d4edc9581e6
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-04-07 09:38:40 +0300

    [FIX] fix PO print lines' column headers and their positioning

--- purchase_report_line_product_internal_reference/__manifest__.py
+++ purchase_report_line_product_internal_reference/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Purchase Order reports - Product's internal reference",
     "summary": "Purchase Order reports - Product's internal reference",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Reporting",
     "website": "https://github.com/tawasta/purchase",
     "author": "Tawasta",
