f1d7cc7c65940cbbd4ccfbadca5645b6a6d92455
difbit <timo.kekalainen@tawasta.fi>
2025-04-08 08:51:45 +0300

    [ADD] purchase_request_from_sale_order_contents module: A new functionality to add sale order contents to purchase request

---
+++ purchase_request_from_sale_order_contents/__manifest__.py
@@ -0,0 +1,39 @@
+##############################################################################
+#
+#    Author: Futural Oy
+#    Copyright 2025 Futural Oy (https://futural.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Purchase Request from Sale Order",
+    "summary": "Add sale order contents to PR lines",
+    "version": "17.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://github.com/tawasta/purchase",
+    "author": "Futural",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase_request",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/sale_to_request.xml",
+        "views/purchase_request.xml",
+    ],
+}
