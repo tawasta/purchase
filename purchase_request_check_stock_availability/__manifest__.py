4238ab1a2953e8db66b02d963a7344fe766426ac
difbit <timo.kekalainen@tawasta.fi>
2025-07-22 12:37:41 +0300

    [IMP] purchase_request_check_stock_availability module: Check if a purchase request line can be deleted

--- purchase_request_check_stock_availability/__manifest__.py
+++ purchase_request_check_stock_availability/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Purchase Request Stock Availability check",
     "summary": "Trigger internal transfers from Purchase Request view",
-    "version": "17.0.2.3.1",
+    "version": "17.0.2.3.2",
     "category": "Purchases",
     "website": "https://github.com/tawasta/purchase",
     "author": "Tawasta",
